7859012c42f390666a9d752251ca5d7e0590fbd6
Maria <mariatc025@gmail.com>
2024-10-31 01:05:29 +0100

    first commit

---
+++ countlines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE=$1
+NLINES=$(cat $FILE | wc -l)
+FNAME=$(basename "$FILE")
+
+if [[ $NLINES -gt 1 ]];
+then echo $FNAME has $NLINES lines;
+elif [[$NLINES -eq 1 ]];
+then echo $FNAME has 1 line;
+else echo $FNAME has 0 lines;
+fi;	
